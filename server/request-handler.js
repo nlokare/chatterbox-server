7fe06e5c956f7b1106766e8d9e5fadaed46745a6
Hack Reactor Students <accounts@hackreactor.com>
2015-01-05 20:21:16 -0800

    Post is now working with proper success messages

--- server/request-handler.js
+++ server/request-handler.js
@@ -3,20 +3,7 @@ var url = require('url');
 var _ = require('underscore');
 
 exports.requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such ands
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   var headers = defaultCorsHeaders;
   console.log("Serving request type " + request.method + " for url " + request.url);
 
@@ -36,24 +23,13 @@ exports.requestHandler = function(request, response) {
       response.end();
     }
     if (request.method === 'POST' || request.method === 'PUT') {
-      headers['Content-Type'] = "application/json";
-      parsePost(request, postMessage);
-      response.writeHead(201, headers);
-      response.end();
+      headers['Content-Type'] = "text/plain";
+      parsePost(request, response, headers, postMessage);
       console.log("message successfully added");
     }
   }
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
@@ -72,12 +48,14 @@ var getMessages = function (query) {
   return {'results': results};
 };
 
-var parsePost = function (req, callback) {
+var parsePost = function (req, res, headers, callback) {
   var data = '';
   req.on('data', function(chunk) {
     data += chunk;
   });
   req.on('end', function() {
+    res.writeHead(201, headers);
+    res.end();
     callback(data);
   });
 };
@@ -89,6 +67,6 @@ var postMessage = function (data) {
   var text = data['text'];
   var roomname = data['roomname'] || 'lobby';
   var date = new Date();
-  messages[messages.length + 1] = {'createdAt' : date, 'username': username, 'text': text, 'roomname': roomname};
+  messages[Object.keys(messages).length + 1] = {'createdAt' : date, 'username': username, 'text': text, 'roomname': roomname};
 };
 
