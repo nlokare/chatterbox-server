f97bc7031de49ec2aa4fa92a6f6f3c85cfd56d04
Hack Reactor Students <accounts@hackreactor.com>
2015-01-06 12:31:08 -0800

    Begins to route index.html file to root URL on server. Almost complete

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,8 +1,10 @@
 var http = require("http");
 
+
 var handleRequest = require('./request-handler.js').requestHandler;
 
 var server = http.createServer(handleRequest);
 console.log("Listening on http://127.0.0.1:3000");
 server.listen(3000, "127.0.0.1");
+
 
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,6 +1,8 @@
 var messages = require('./messages.js').messages;
 var url = require('url');
 var _ = require('underscore');
+var fs = require('fs');
+var Path = require('path');
 
 exports.requestHandler = function(request, response) {
 
@@ -10,15 +12,19 @@ exports.requestHandler = function(request, response) {
   var query = url.parse(request.url, true).query;
   var path = url.parse(request.url, true).pathname;
 
-  if (path !== "/classes/messages" && path !== "/send") {
-    response.writeHead(404, headers);
-    response.end('Does not exist!');
+  if(path === "/" || path.indexOf("/client") >= 0){
+    app(request, response, path);
   } else {
-    if (request.method === 'GET' || request.method === 'OPTIONS') {
-      getMessages(request, response, headers, query);
-    }
-    if (request.method === 'POST' || request.method === 'PUT') {
-      parsePost(request, response, headers, postMessage);
+    if (path !== "/classes/messages" && path !== "/send") {
+      response.writeHead(404, headers);
+      response.end('Does not exist!');
+    } else {
+      if (request.method === 'GET' || request.method === 'OPTIONS') {
+        getMessages(request, response, headers, query);
+      }
+      if (request.method === 'POST' || request.method === 'PUT') {
+        parsePost(request, response, headers, postMessage);
+      }
     }
   }
 };
@@ -29,6 +35,30 @@ var defaultCorsHeaders = {
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10 // Seconds.
 };
+
+var app = function(req, res, filename){
+  var file;
+  if(filename === "/"){
+    file = Path.resolve(__dirname, '..', 'client/index.html');
+
+  } else {
+    // filename = filename.slice(1);
+    file = Path.resolve(__dirname, '..', filename);
+  }
+  console.log(file);
+  console.log(__dirname);
+  fs.readFile(file, 'binary', function(error, content) {
+    if (error) {
+      res.writeHead(500);
+      res.end();
+    }
+    else {
+      res.writeHead(200, { 'Content-Lenth': content.length });
+      res.end(content);
+    }
+  });
+};
+
 
 var getMessages = function (request, response, headers, query) {
   headers['dataType'] = 'json';
