5d0682b56387f123ca81fb75b92e123493dbf868
Hack Reactor Students <accounts@hackreactor.com>
2015-01-06 21:00:07 -0800

    Sets up host variable

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,11 +1,12 @@
 var http = require("http");
 var port = process.env.PORT || 3000;
+var host = process.env.HOST || '0.0.0.0';
 
 
 var handleRequest = require('./request-handler.js').requestHandler;
 
 var server = http.createServer(handleRequest);
 console.log("Listening on http://127.0.0.1:3000");
-server.listen(port, "nzchat.azurewebsites.net");
+server.listen(port, host);
 
 
