7f713c2ec5ddd4a6655707da9b58f1a2417c21f8
Neil Lokare <neil.lokare@gmail.com>
2015-01-08 15:23:53 -0800

    Delets node_modules folder and testing suites

--- client/env/config.js
+++
@@ -1,9 +0,0 @@
-//YOU DO NOT NEED TO EDIT this code.
-if (!/(&|\?)username=/.test(window.location.search)) {
-  var newSearch = window.location.search;
-  if (newSearch !== '' & newSearch !== '?') {
-    newSearch += '&';
-  }
-  newSearch += 'username=' + (prompt('What is your name?') || 'anonymous');
-  window.location.search = newSearch;
-}
